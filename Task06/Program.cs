8ff6b328dabbdde05cf8d5fc86a5ab54a5211b57
Lakshmy <lovely_lakshmy@mail.ru>
2022-12-07 15:50:29 +0300

    Задача 06

---
+++ Task06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.Write("Введите число: ");
+            int.TryParse(Console.ReadLine(), out a);
+
+            if (a % 2 == 0)
+                Console.WriteLine("Введенное число четное");
+            else
+                Console.WriteLine("Введенное число нечетное");
+        }
+    }
+}
