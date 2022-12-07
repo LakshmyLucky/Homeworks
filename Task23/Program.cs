aac1dc837973b843e9fc2faf3ce99ecfffa67749
Lakshmy <lovely_lakshmy@mail.ru>
2022-12-07 17:39:33 +0300

    Задача 23

---
+++ Task23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Введите число n, отличное от нуля: ");
+            int.TryParse(Console.ReadLine(), out n);
+
+            if (n == 0)
+            {
+                Console.WriteLine("Ошибка преобразования числа");
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+                Console.Write("{0} ", Math.Pow(i, 3));
+
+            Console.WriteLine();
+        }
+    }
+}
