fba699a87b722484409bbf6911a2000b2c8f18fc
Lakshmy <lovely_lakshmy@mail.ru>
2022-12-07 15:46:22 +0300

    Задача 04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -10,6 +10,24 @@ namespace Task04
     {
         static void Main(string[] args)
         {
+            int a, b, c, max;
+
+            Console.Write("Введите число a: ");
+            int.TryParse(Console.ReadLine(), out a);
+            Console.Write("Введите число b: ");
+            int.TryParse(Console.ReadLine(), out b);
+            Console.Write("Введите число c: ");
+            int.TryParse(Console.ReadLine(), out c);
+
+            if (a == b & a == c)
+                Console.WriteLine("Числа равны");
+            else
+            {
+                max = a;
+                if (b > max) max = b;
+                if (c > max) max = c;
+                Console.WriteLine("max = {0}", max);
+            }
         }
     }
 }
