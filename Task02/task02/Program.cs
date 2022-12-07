7514f66ee25a884bd7bc7fda718abbf487f05c4d
Lakshmy <lovely_lakshmy@mail.ru>
2022-12-07 15:16:41 +0300

    Задача 02

---
+++ Task02/task02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a, b;
+
+            Console.Write("Введите число a: ");
+            byte.TryParse(Console.ReadLine(), out a);
+            Console.Write("Введите число b: ");
+            byte.TryParse(Console.ReadLine(), out b);
+
+            if (a == b)
+                Console.WriteLine("Числа равны");
+            else
+                if (a > b)
+                    Console.WriteLine("max = {0}", a);
+                else
+                    Console.WriteLine("max = {0}", b);
+        }
+    }
+}
