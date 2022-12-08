a72598ce392111e772aaaad60f97d9bfa31527f5
Lakshmy <lovely_lakshmy@mail.ru>
2022-12-09 02:31:26 +0300

    Задача 29

---
+++ Task29/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] array = new byte[8];
+
+            Console.WriteLine("Введите массив из 8 элементов:");
+
+            for (int i = 0; i < array.Length; i++)
+                byte.TryParse(Console.ReadLine(), out array[i]);
+
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0}", array[i]);
+
+                //Просто для красоты, чтобы лишнего пробела перед ] не было :)
+                if (i != array.Length - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
