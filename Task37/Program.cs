27239c91fbf75fe79d264b0590dcb78403c49096
Lakshmy <lovely_lakshmy@mail.ru>
2022-12-19 00:13:53 +0300

    Задача 37

---
+++ Task37/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task37
+{
+ 
+    class Program
+    {
+
+        static int[] InputArray(int[] arr)
+        {
+            //Честно - с рандомом в # ни разу не работала, не знаю как правильно
+            Random random = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = random.Next(100, 999);
+
+            return arr;
+        }
+
+        static void OutputArray(int[] arr)
+        {
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i].ToString());
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+        static void OutputEvenArray(int[] arr)
+        {
+            byte sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] % 2 == 0) sum++;
+            Console.Write("Количество чётных чисел в массиве = ");
+            Console.WriteLine(sum);
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            //Фомирование массива
+            InputArray(array);
+            //Вывод массива
+            OutputArray(array);
+            //Вывод результата
+            OutputEvenArray(array);
+        }
+    }
+}
