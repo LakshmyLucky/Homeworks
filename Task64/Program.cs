018e91d4f6c3c4a2fc432dd30ccb00ec68332ad0
Lakshmy <lovely_lakshmy@mail.ru>
2023-01-14 10:03:01 +0300

    Задача 64

---
+++ Task64/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task64
+{
+    class Program
+    {
+        static void Recursion(int n)
+        {
+            if (n < 1) return;
+
+            Console.Write(n + " ");
+            Recursion(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int n;
+            int.TryParse(Console.ReadLine(), out n);
+            Recursion(n);
+            Console.WriteLine();
+        }
+    }
+}
