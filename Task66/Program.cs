5dd44ff2aaeb971269d30362fb34535f313928ff
Lakshmy <lovely_lakshmy@mail.ru>
2023-01-14 10:03:50 +0300

    Задача 66

---
+++ Task66/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task66
+{
+    class Program
+    {
+        static int Recursion(int m, int n)
+        {
+            if (m == n) return m;
+            return m + Recursion(m + 1, n);
+        }
+        static void Main(string[] args)
+        {
+            int m, n;
+            Console.Write("Введите число M: ");
+            int.TryParse(Console.ReadLine(), out m);
+            Console.Write("Введите число N: ");
+            int.TryParse(Console.ReadLine(), out n);
+            Console.WriteLine(Recursion(m, n));
+        }
+    }
+}
