766b438d90e2bc1efc07758ac02f6f0d1c0ecece
Lakshmy <lovely_lakshmy@mail.ru>
2023-01-14 10:04:43 +0300

    Задача 68

---
+++ Task68/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task68
+{
+    class Program
+    {
+        static int Recursion(int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if (m > 0 && n == 0)
+            {
+                return Recursion(m - 1, 1);
+            }
+            else
+            {
+                return Recursion(m - 1, Recursion(m, n - 1));
+            }
+        }
+        static void Main(string[] args)
+        {
+            int m, n;
+            Console.Write("Введите число M: ");
+            int.TryParse(Console.ReadLine(), out m);
+            Console.Write("Введите число N: ");
+            int.TryParse(Console.ReadLine(), out n);
+            Console.WriteLine(Recursion(m, n));
+        }
+    }
+}
